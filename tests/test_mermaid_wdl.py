110d4b9a35630f8bb02d20e8c4bd420f2a43cde2
Ryan Lim <rlim@chanzuckerberg.com>
2023-08-30 13:52:58 -0700

    add tests

---
+++ tests/test_mermaid_wdl.py
@@ -0,0 +1,47 @@
+from tests.test_miniwdl_parser import simple_wdl, complex_wdl
+from miniwdl_viz.mermaid_wdl import MermaidWDL
+from os.path import join, dirname, realpath
+import hashlib
+import os
+
+class TestMermaidWDL:
+    def test_simple_mermaid_wdl(self, simple_wdl):
+        output_filename = join(dirname(realpath(__file__)), "simple.mmd")
+        mw = MermaidWDL(
+            flowchart_dir="LR",
+            suppress_workflow_input=False,
+            suppress_hardcoded_variables=True,
+            output_name= output_filename
+        )
+        mermaid_list = mw.create_mermaid_flowchart(simple_wdl.nodes, simple_wdl.edges)
+        assert mermaid_list[1] == 'WorkflowInput(("WorkflowInput")) --> |input_file, docker_image_id| call-add_world{{"add_world"}}'
+        assert mermaid_list[2] == 'call-add_world{{"add_world"}} --> |input_file| call-add_goodbye{{"add_goodbye"}}'
+
+        mw.output_mermaid(mermaid_list)
+        with open(output_filename, "r") as f:
+            file = f.read()
+        hash = hashlib.md5(file.encode())
+        assert hash.hexdigest() == "5447602150e9b75914b5fad0da96bf09"
+        os.remove(output_filename)
+
+    def test_complex_mermaid_wdl(self, complex_wdl):
+        output_filename = join(dirname(realpath(__file__)), "complex.mmd")
+        mw = MermaidWDL(
+            flowchart_dir="LR",
+            suppress_workflow_input=False,
+            suppress_hardcoded_variables=True,
+            output_name= output_filename
+        )
+        mermaid_list = mw.create_mermaid_flowchart(complex_wdl.nodes, complex_wdl.edges)
+        assert mermaid_list[1] == 'subgraph scatter-L120C3-idx["range(length(unpadded_intervals))"]'
+        assert mermaid_list[-1] == 'WorkflowInput(("WorkflowInput")) --> |output_prefix, disk_size, docker, gatk_path| call-GatherMetrics{{"GatherMetrics"}}'
+
+        mw.output_mermaid(mermaid_list)
+        with open(output_filename, "r") as f:
+            file = f.read()
+        hash = hashlib.md5(file.encode())
+        assert hash.hexdigest() == "70d17c20397b15e1ddc0b360ec98fa5c"
+        os.remove(output_filename)
+
+
+        
---
+++ tests/test_miniwdl_parser.py
@@ -0,0 +1,34 @@
+from miniwdl_viz.miniwdl_parser2 import MiniWDLParser2
+from os.path import dirname, realpath, join
+import WDL
+import pytest
+
+@pytest.fixture
+def simple_wdl():
+    doc = WDL.load(join(dirname(realpath(__file__)), "test_wdls", "simple.wdl"))
+    parser = MiniWDLParser2(doc)
+    parser.parse()
+    return parser
+
+@pytest.fixture
+def complex_wdl():
+    doc = WDL.load(join(dirname(realpath(__file__)), "test_wdls", "joint-discovery-gatk4-version.wdl"))
+    parser = MiniWDLParser2(doc)
+    parser.parse()
+    return parser
+
+class TestMiniWDLParser:
+
+    def test_simple_wdl(self, simple_wdl):
+        assert len(simple_wdl.nodes) == 3
+        assert len(simple_wdl.edges) == 6
+    
+    def test_complex_wdl(self, complex_wdl):
+
+        call_nodes = [node for node in complex_wdl.nodes if node["type"] == "call"]
+        subsection_nodes = [node for node in complex_wdl.nodes if node["type"] == "workflow_section"]
+        decl_nodes = [node for node in complex_wdl.nodes if node["type"] == "decl"]
+
+        assert len(call_nodes) == 15
+        assert len(subsection_nodes) == 8
+        assert len(decl_nodes) == 2
